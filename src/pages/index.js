ff620092c3ebf4fc9e5045f0f96f2e129b0a77df
JeremyCrichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-08-10 13:45:11 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <h1>Updated August 10th 2020 - Staging 5</h1>
+      <h1>Updated August 10th 2020 - Staging 5 updated on GH</h1>
       <SEO title="All posts" />
       <Bio />
       {posts.map(({ node }) => {
