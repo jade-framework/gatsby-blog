cab73aa9906e0beabd7195228a1e1facb503f5ab
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-08-10 22:11:26 -0700

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <h1>Updated August 10 2020 - 19</h1>
+      <h1>Updated August 10 2020 - 20</h1>
       <SEO title="All posts" />
       <Bio />
       {posts.map(({ node }) => {
