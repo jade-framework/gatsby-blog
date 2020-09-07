029e8df29e2cad5329d6285d602de0626a76d068
Krunal Patel <48189112+kpatel27@users.noreply.github.com>
2020-09-07 09:54:23 -0700

    Update index.js
    testing automated build

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <h1>Updated Sep 7 2020 - 2</h1>
+      <h1>Updated Sep 7 2020 - 3</h1>
       <SEO title="All posts" />
       <Bio />
       {posts.map(({ node }) => {
